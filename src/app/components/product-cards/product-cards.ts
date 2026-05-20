a9f850c180af5c45f29b7e292af7b6deebf9aaaf
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-05-20 10:30:45 +0300

    filestructureStart

---
+++ src/app/components/product-cards/product-cards.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-cards',
+  imports: [],
+  templateUrl: './product-cards.html',
+  styleUrl: './product-cards.css',
+})
+export class ProductCards {}
